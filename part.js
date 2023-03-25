ea8c67b224a4ffe4b9c0428d80bc09ff31e29289
Cian Chambliss <cianchambliss@Cians-Mac-mini.local>
2023-03-25 10:53:36 -0400

    definition of parts

--- part.js
+++ part.js
@@ -18,13 +18,55 @@ module.exports = function(singleton) {
             }
             if( args.plural ) {
                 _part.plural = true;
+                singleton.outputText("Ok, "+args.subject+" are part of every "+args.type);
+            } else {
+                singleton.outputText("Ok, "+args.subject+" is part of every "+args.type);
             }
-            console.dir("Ok, "+args.subject+" is part of every "+args.type);
         } else if( args.dObj ) {
-            console.log("TBD...");
-
+            var npc = singleton.findNPC(args.dObj);
+            if( npc ) {
+                if( !npc.parts ) {
+                    npc.parts = {};
+                }
+                var _part = npc.parts[args.subject];
+                if( !_part ) {
+                    _part = { name : args.subject };
+                    npc.parts[args.subject] = _part;
+                }
+                if( args.plural ) {
+                    _part.plural = true;
+                    singleton.outputText("Ok, "+args.subject+" are part of "+args.dObj);
+                } else {
+                    singleton.outputText("Ok, "+args.subject+" is part of "+args.dObj);
+                }
+            } else {
+                var objName = singleton.lookupItem(game.pov.location,args.dObj);
+                if( objName && objName != "?") {
+                    var _item = game.getItem(objName);
+                    if( _item ) {
+                        if( !_item.parts ) {
+                            _item.parts = {};
+                        }
+                        var _part = _item.parts[args.subject];
+                        if( !_part ) {
+                            _part = { name : args.subject };
+                            _item.parts[args.subject] = _part;
+                        }
+                        if( args.plural ) {
+                            _part.plural = true;
+                            singleton.outputText("Ok, "+args.subject+" are part of "+args.dObj);
+                        } else {
+                            singleton.outputText("Ok, "+args.subject+" is part of "+args.dObj);
+                        }
+                    } else  {
+                        singleton.dontSee(args.dObj,game.pov.location);
+                    }               
+                } else if( !objName ) {
+                    singleton.dontSee(args.dObj,game.pov.location);
+                }
+            }
         } else {
-            console.log("Must supply a type or an object or npc");
+            singleton.outputText("Must supply a type or an object or npc");
         }
     };
     return { definePart : definePart };
